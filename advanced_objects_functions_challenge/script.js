da62426fdbcd4fde3f7162637abe706c39775f78
David Vivó <david.vivo@oramon.nl>
2020-04-23 17:10:24 +0200

    Create questions constructor

--- advanced_objects_functions_challenge/script.js
+++ advanced_objects_functions_challenge/script.js
@@ -19,4 +19,11 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-console.log('Hello World');
+
+
+var Question = function (question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+};
+
