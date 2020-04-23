6cfe6723b232851a5800d9470abaadf1f5d79e22
David Vivó <david.vivo@oramon.nl>
2020-04-23 17:12:27 +0200

    Create questions and store them in array

--- advanced_objects_functions_challenge/script.js
+++ advanced_objects_functions_challenge/script.js
@@ -19,11 +19,44 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-
-
-var Question = function (question, answers, correctAnswer) {
+var Question = function (question, answers, userAnswer, correctAnswer) {
     this.question = question;
     this.answers = answers;
     this.correctAnswer = correctAnswer;
+    this.userAnswer = userAnswer;
 };
 
+var question1 = new Question(
+    'Is the World square?',
+    [
+        '0: Yes',
+        '1: No'
+    ],
+    undefined,
+    0
+);
+
+var question2 = new Question(
+    'Which are the water\'s estates?',
+    [
+        '0: Liquid and Solid',
+        '1: Solid and Gas',
+        '2: Liquid, Solid and Gas'
+    ],
+    undefined,
+    2
+);
+
+var question3 = new Question(
+    'What is the direction of Time',
+    [
+        '0: Back',
+        '1: Forward',
+        '2: Both'
+    ]
+    ,
+    undefined,
+    1
+);
+
+var questions = [question1, question2, question3];
