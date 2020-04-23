bf8959b8cede43ad34126dbc7de35e52869068da
David Vivó <david.vivo@oramon.nl>
2020-04-23 19:03:46 +0200

    Wrap in IIFE

--- advanced_objects_functions_challenge/script.js
+++ advanced_objects_functions_challenge/script.js
@@ -19,70 +19,73 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-var Question, questions, question1, question2, question3, currentQuestion
+(function () {
+    var Question, questions, question1, question2, question3, currentQuestion
 
-Question = function (question, answers, userAnswer, correctAnswer) {
-    this.question = question;
-    this.answers = answers;
-    this.correctAnswer = correctAnswer;
-    this.userAnswer = userAnswer;
-};
+    Question = function (question, answers, userAnswer, correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+        this.userAnswer = userAnswer;
+    };
 
-Question.prototype.logQuestion = function () {
-    console.log(this.question);
-    for (var i = 0; i < this.answers.length; i++) {
-        console.log(this.answers[i])
-    }
-};
+    Question.prototype.logQuestion = function () {
+        console.log(this.question);
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(this.answers[i])
+        }
+    };
 
-Question.prototype.obtainUserAnswer = function () {
-    this.userAnswer = parseInt(prompt(this.question));
-};
+    Question.prototype.obtainUserAnswer = function () {
+        this.userAnswer = parseInt(prompt(this.question));
+    };
 
-Question.prototype.checkUserAnswer = function () {
-    if (this.userAnswer === this.correctAnswer) {
-        console.log('YES, IS CORRECT YOU SMARTY PANTS!');
+    Question.prototype.checkUserAnswer = function () {
+        if (this.userAnswer === this.correctAnswer) {
+            console.log('YES, IS CORRECT YOU SMARTY PANTS!');
 
-    } else {
-        console.log('NO WAY, YOU SCIENTOLOGIST');
-    }
-};
+        } else {
+            console.log('NO WAY, YOU SCIENTOLOGIST');
+        }
+    };
 
-question1 = new Question(
-    'Is the World square?',
-    [
-        '0: Yes',
-        '1: No'
-    ],
-    undefined,
-    1
-);
+    question1 = new Question(
+        'Is the World square?',
+        [
+            '0: Yes',
+            '1: No'
+        ],
+        undefined,
+        1
+    );
 
-question2 = new Question(
-    'Which are the water\'s estates?',
-    [
-        '0: Liquid and Solid',
-        '1: Solid and Gas',
-        '2: Liquid, Solid and Gas'
-    ],
-    undefined,
-    2
-);
+    question2 = new Question(
+        'Which are the water\'s estates?',
+        [
+            '0: Liquid and Solid',
+            '1: Solid and Gas',
+            '2: Liquid, Solid and Gas'
+        ],
+        undefined,
+        2
+    );
 
-question3 = new Question(
-    'What is the direction of Time',
-    [
-        '0: Back',
-        '1: Forward',
-        '2: Both'
-    ]
-    ,
-    undefined,
-    1
-);
+    question3 = new Question(
+        'What is the direction of Time',
+        [
+            '0: Back',
+            '1: Forward',
+            '2: Both'
+        ]
+        ,
+        undefined,
+        1
+    );
 
-questions = [question1, question2, question3];
-currentQuestion = Math.floor(Math.random() * questions.length);
-questions[currentQuestion].logQuestion();
-questions[currentQuestion].obtainUserAnswer();
-questions[currentQuestion].checkUserAnswer();
+    questions = [question1, question2, question3];
+    currentQuestion = Math.floor(Math.random() * questions.length);
+    questions[currentQuestion].logQuestion();
+    questions[currentQuestion].obtainUserAnswer();
+    questions[currentQuestion].checkUserAnswer();
+
+})();
